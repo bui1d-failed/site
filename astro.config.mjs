cff44dc0ce45348ca408c3d17bde20f9491ee41f
build failed <krill.girdles4n@icloud.com>
2025-12-14 15:10:33 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://bui1d-failed.github.io/",
-	base: "/",
+	site: "https://bui1d-failed.github.io",
+	base: "/site",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
