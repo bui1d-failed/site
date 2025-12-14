719af565e84d282437698e5d1086ee33633cbbff
build failed <krill.girdles4n@icloud.com>
2025-12-14 15:55:02 +0000

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -54,7 +54,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Build Failed",
+	name: "build-failed",
 	bio: "hi.",
 	links: [
 		{
